c5110ce576852c8716993aad51b98f545144f9a4
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-07-11 06:04:07 +0300

    build: add dotenv, dotenv dist file

--- src/config.py
+++ src/config.py
@@ -1,8 +1,16 @@
 import os
+from os.path import dirname, join
+
+import dotenv
+
+SRC_ROOT = dirname(__file__)
+PROJECT_ROOT = join(SRC_ROOT, "..")
+
+dotenv.load_dotenv(join(PROJECT_ROOT, ".env"))
 
 DATABASE = {
     "URL": os.environ.get("DB_URL", "postgresql+psycopg2://atom:atom@localhost:5432/atom"),
-    "DEBUG": os.environ.get("DB_DEBUG", False)
+    "DEBUG": os.environ.get("DB_DEBUG", False).lower() == "true"
 }
 
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = os.environ.get("DEBUG", False).lower() == "true"
